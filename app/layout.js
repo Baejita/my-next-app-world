cf9511392466c8de48bd41d57afe8b0b6873c93b
Baejita <homokoro@gmail.com>
2024-08-07 20:30:23 +0700

    complete Navbar

--- app/layout.js
+++ app/layout.js
@@ -5,12 +5,17 @@ export const metadata = {
   title: "Property Pulse",
   keywords: "rentalm property, real estate",
   description: "Find the perfect rental",
+  icons: {
+    icon: "/favicon.ico",
+  },
 };
 
 export default function RootLayout({ children }) {
   return (
     <html>
       <body>
+        <link rel="icon" href="/favicon.ico" sizes="32x32" />
+
         <Navbar />
         <main>{children}</main>
       </body>
